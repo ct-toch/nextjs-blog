dfe5282b73ba449fbab7f25a1bc5c810a681d56b
Chhay Toch <chhaytoch.dev@gmail.com>
2021-10-30 20:58:18 +0700

    Project Born

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Learn App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <Link href="/posts/first-post">
+          <a>First Post</a>
+        </Link>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
@@ -54,8 +59,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
@@ -205,5 +209,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
---
+++ pages/posts/first-post.js
@@ -0,0 +1,10 @@
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <h1>First Post</h1>
+      <a>Hello World</a>
+    </Layout>
+  );
+}
